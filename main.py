036c59e45d64eeac1235f330a41eaae2d55d4c9f
Carrick <A00303154>
2025-01-14 00:27:07 +0000

    first

---
+++ main.py
@@ -0,0 +1,29 @@
+#pip install python-vlc
+
+import tkinter as tk
+from vlc_player import VLCPlayer
+from playback_controls.py import PlaybackControls
+
+
+class MainApp:
+    """Main application to combine all components."""
+    def __init__(self, root):
+        self.root = root
+        self.root.title("VLC Music Player")
+        self.root.geometry("400x250")
+
+        self.player = VLCPlayer()
+        self.time_label_widget = tk.Label(self.root, text="00:00 / 00:00")
+        self.time_label_widget.grid(row=3, column=0, columnspan=4, pady=10)
+
+        self.controls = PlaybackControls(
+            root,
+            self.player,
+            lambda current, duration: TimeLabel.update_time_label(self.time_label_widget, current, duration)
+        )
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = MainApp(root)
+    root.mainloop()
---
+++ vlc_player.py
@@ -0,0 +1,41 @@
+import vlc
+
+
+class VLCPlayer:
+    """Handles the VLC player functionality."""
+    def __init__(self):
+        self.instance = vlc.Instance()
+        self.player = self.instance.media_player_new()
+        self.media = None
+
+    def load_media(self, file_path):
+        self.media = self.instance.media_new(file_path)
+        self.player.set_media(self.media)
+
+    def play(self):
+        if self.media:
+            self.player.play()
+
+    def pause(self):
+        self.player.pause()
+
+    def stop(self):
+        self.player.stop()
+
+    def set_volume(self, volume):
+        self.player.audio_set_volume(int(volume))
+
+    def seek(self, percentage):
+        if self.media:
+            duration = self.get_duration()
+            new_time = (percentage / 100) * duration
+            self.player.set_time(int(new_time * 1000))  # Set time in milliseconds
+
+    def get_time(self):
+        return self.player.get_time() / 1000  # Current time in seconds
+
+    def get_duration(self):
+        return self.player.get_length() / 1000  # Total duration in seconds
+
+    def is_playing(self):
+        return self.player.is_playing()
